9b5554e2be0c81ea3553cbf5fb00fe5808972715
Tero Mäntylä <termanty@cs.helsinki.fi>
2014-05-19 10:29:16 +0300

    NetBeans Maven projektin luonti

---
+++ JShakki/src/main/java/jshakki/jshakki/App.java
@@ -0,0 +1,13 @@
+package jshakki.jshakki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
