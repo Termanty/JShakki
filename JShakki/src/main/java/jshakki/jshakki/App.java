c4f5998885ee696495d808b2d5fc9b05b28128aa
Tero Mäntylä <termanty@cs.helsinki.fi>
2014-05-19 14:18:13 +0300

    koodauksen aloitus

--- JShakki/src/main/java/jshakki/jshakki/App.java
+++ JShakki/src/main/java/jshakki/jshakki/App.java
@@ -1,13 +1,15 @@
 package jshakki.jshakki;
 
 /**
- * Hello world!
- *
+ * 
+ * 
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+        new RunGame().run();
+       
     }
 }
